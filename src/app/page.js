2590d3beb9fbddb726a0c234e92638315b10827a
ESinson <70695462+EDS435@users.noreply.github.com>
2023-09-22 19:54:28 -0400

    New line name

--- src/app/page.js
+++ src/app/page.js
@@ -98,7 +98,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            New code{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -107,6 +107,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
       </div>
     </main>
   )
