8715565d01f7366a6eb1f521a53e5ee452b154ab
ESinson <70695462+EDS435@users.noreply.github.com>
2023-09-29 18:56:51 -0400

    New tabs added

--- src/pages/pages1.js
+++ src/pages/pages1.js
@@ -147,7 +147,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            New component added for github.
+            New
           </p>
         </a>
       </div>
